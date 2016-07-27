a51c464df5575edba34f013b7f9cce001638d565
Michael Drewitt <mikedrewitt@gmail.com>
2016-07-27 11:59:35 -0400

    broken stuff Revert "Added buttons for Autograder to upload .tar and makefile"
    This reverts commit c57907852b1f196dcafeb44ca833d9646449c20d.

--- spec/controllers/my_controller_controller_spec.rb
+++ spec/controllers/my_controller_controller_spec.rb
@@ -9,25 +9,4 @@ RSpec.describe MyControllerController, type: :controller do
     end
   end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
